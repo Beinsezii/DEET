be9ae534b1ad5a402a8e4ed081c1763ac5563fd7
Beinsezii <beinsezii@gmail.com>
2025-10-26 04:15:50 -0700

    Add model output modes, DEET manager class, fix NaNs

--- demo.py
+++ demo.py
@@ -13,19 +13,15 @@ from PIL import Image, ImageDraw
 from tqdm import tqdm
 from transformers.models.clip import CLIPTextModel, CLIPTokenizer
 
-from diffusion_eet import deet_upcast as deet
+from diffusion_eet import DEET, DEETMode
 
 
 @enum.unique
-class DeetMode(enum.StrEnum):
-    SAMPLE = enum.auto()
-    "deet(sample, sample⁻¹)"
+class DEETModeP(enum.StrEnum):
     PREDICTION = enum.auto()
     "deet(prediction, prediction⁻¹)"
     UNPREDICT = enum.auto()
     "deet(prediction, sample)"
-    RENOISE = enum.auto()
-    "deet(output, sample)"
 
 
 with torch.inference_mode():
@@ -37,6 +33,8 @@ with torch.inference_mode():
     cfg: float = 8
     prompt: str = "masterpiece, fantasy artwork, tabby kitten, rainbow flowers"
     negative: str = "blurry, noisy, cropped"
+
+    use_context_manager: bool = True
 
     schedule: scheduling.SkrampleSchedule = scheduling.Karras(scheduling.Scaled())
     sampler: structured.SkrampleSampler = structured.DPM(order=2)
@@ -65,26 +63,47 @@ with torch.inference_mode():
     scheudle_np = schedule.schedule(steps)
 
     @torch.inference_mode()
-    def sample_deet(mode: DeetMode, deviation: float) -> Image.Image:
+    def sample_deet(mode: DEETMode | DEETModeP, config: DEET) -> Image.Image:
         sample: torch.Tensor = torch.randn([1, 4, 80, 80], generator=seed.clone_state()).to(dtype=dtype, device=device)
         previous: list[structured.SKSamples[torch.Tensor]] = []
+
+        prev_output: torch.Tensor | None = None
+        prev_sample: torch.Tensor | None = None
+        prev_prediction: torch.Tensor | None = None
+
+        if use_context_manager and mode in DEETMode:
+            ctx = config.hook_module(model, mode=mode)  # type: ignore
+            ctx.__enter__()
+            mode = ""  # type: ignore
+
         for n, (timestep, sigma) in enumerate(tqdm(scheudle_np.tolist())):
-            if mode == DeetMode.SAMPLE and previous:
-                sample = deet(sample, previous[-1].sample, deviation)
+            if mode == DEETMode.INPUT and prev_sample is not None:
+                new_sample = config(sample, prev_sample)
+                prev_sample, sample = sample, new_sample
+            else:
+                prev_sample = sample
 
             output = model(sample.repeat((2, 1, 1, 1)), timestep, text_embeds).sample
 
-            if mode == DeetMode.RENOISE:
-                output = deet(output, sample, deviation)
+            if mode == DEETMode.OUTPUT and prev_output is not None:
+                new_output = config(output, prev_output)
+                prev_output, output = output, new_output
+            elif mode == DEETMode.BACKWARD:
+                output = config(output, sample)
+            else:
+                prev_output = output
 
             conditioned, unconditioned = output.chunk(2)
             guided: torch.Tensor = conditioned + (cfg - 1) * (conditioned - unconditioned)
             prediction = predictor(sample, guided, sigma, schedule.sigma_transform)
 
-            if mode == DeetMode.PREDICTION and previous:
-                prediction = deet(prediction, previous[-1].prediction, deviation)
-            elif mode == DeetMode.UNPREDICT:
-                prediction = deet(prediction, sample, deviation)
+            if mode == DEETModeP.PREDICTION and prev_prediction is not None:
+                new_prediction = config(prediction, prev_prediction)
+                prev_prediction, prediction = prediction, new_prediction
+            elif mode == DEETModeP.UNPREDICT:
+                prediction = config(prediction, sample)
+            else:
+                prev_prediction = prediction
 
             sampler_output = sampler.sample(
                 sample=sample,
@@ -98,17 +117,25 @@ with torch.inference_mode():
             previous.append(sampler_output)
             sample = sampler_output.final
 
+        if use_context_manager and mode in DEETMode:
+            ctx.__exit__()  # type: ignore
+
         image: torch.Tensor = image_encoder.decode(sample / image_encoder.config.scaling_factor).sample[0]  # type: ignore
         srgb = Image.fromarray(
             ((image + 1) * (255 / 2)).clamp(0, 255).permute(1, 2, 0).to(device="cpu", dtype=torch.uint8).numpy()
         )
         return srgb
 
-    with cf.ThreadPoolExecutor(4) as pool:
-        devs: tuple[float, ...] = (2, 1.1, 1, 0.9, 0.5, 0.1, 0, -0.1, -0.5, -0.9, -1, -1.1, -2)
-        # devs: tuple[float, ...] = (0, 0.5, 1, 1.5, 2)
-        modes: tuple[DeetMode, ...] = (DeetMode.SAMPLE, DeetMode.RENOISE, DeetMode.PREDICTION, DeetMode.UNPREDICT)
-        deets: tuple[tuple[DeetMode, float], ...] = tuple((m, d) for d in devs for m in modes)
+    with cf.ThreadPoolExecutor(1 if use_context_manager else 4) as pool:
+        configs: tuple[DEET, ...] = (
+            DEET(0, 1, False),
+            DEET(1.5, 1, False),
+            DEET(2, 2, True),
+            DEET(3, 0.5, False),
+            DEET(-2, -1, False),
+        )
+        modes: tuple[DEETMode | DEETModeP, ...] = tuple(DEETMode) + tuple(DEETModeP)
+        deets: tuple[tuple[DEETMode | DEETModeP, DEET], ...] = tuple((m, d) for d in configs for m in modes)
 
         images: list[cf.Future[Image.Image]] = [pool.submit(sample_deet, mode, dev) for (mode, dev) in deets]
 
@@ -119,9 +146,9 @@ with torch.inference_mode():
         fill = (255, 255, 255)
         stroke_fill = (0, 0, 0)
 
-        canvas = Image.new("RGB", (width * len(modes), height * len(devs)), (0, 0, 0))
+        canvas = Image.new("RGB", (width * len(modes), height * len(configs)), (0, 0, 0))
         for (m, d), image in zip(deets, tqdm(images, desc="Total"), strict=True):
-            x, y = (modes.index(m) * width, devs.index(d) * height)
+            x, y = (modes.index(m) * width, configs.index(d) * height)
             canvas.paste(image.result(), (x, y))
 
             # X labels
@@ -141,7 +168,7 @@ with torch.inference_mode():
                 draw = ImageDraw.Draw(canvas)
                 draw.text(
                     (pad, y + height - font_size - pad),
-                    f"deviation: {d}",
+                    f"DEET({d.deviation}, {d.power}, {d.invert})",
                     fill=fill,
                     stroke_fill=stroke_fill,
                     font_size=font_size,
--- src/diffusion_eet/__init__.py
+++ src/diffusion_eet/__init__.py
@@ -1,6 +1,21 @@
+import contextlib
+import dataclasses
+import enum
+import inspect
 import math
+from collections.abc import Generator, MutableMapping, MutableSequence
+from typing import TYPE_CHECKING, Any
 
 import torch
+
+if TYPE_CHECKING:
+    from torch.utils.hooks import RemovableHandle
+
+
+def spowf[T: torch.Tensor | float](x: T, f: float) -> T:
+    """Computes x^f in absolute then re-applies the sign to stabilize chaotic inputs.
+    More computationally expensive than plain `math.pow`"""
+    return abs(x) ** f * (-1 * (x < 0) | 1)  # type: ignore
 
 
 def deet(
@@ -19,23 +34,143 @@ def deet(
     else:
         return low_noise
 
-    mask = (mask**power).clamp(0, 1)
+    mask = spowf(mask, power).clamp(0, 1)
     return low_noise * (1 - mask) + high_noise * mask * (invert * -1 | 1)
 
 
-def deet_upcast(
-    low_noise: torch.Tensor,
-    high_noise: torch.Tensor,
-    deviation: float = 1,
-    power: float = 1,
-    invert: bool = False,
-    *,
-    compute_scale: torch.dtype = torch.float64,
-) -> torch.Tensor:
-    return deet(
-        low_noise.to(dtype=compute_scale),
-        high_noise.to(dtype=compute_scale),
-        deviation,
-        power,
-        invert,
-    ).to(dtype=low_noise.dtype)
+@enum.unique
+class DEETMode(enum.StrEnum):
+    INPUT = enum.auto()
+    "deet(input, input⁻¹)"
+    OUTPUT = enum.auto()
+    "deet(output, output⁻¹)"
+    BACKWARD = enum.auto()
+    "deet(output, input)"
+
+
+@dataclasses.dataclass(frozen=True)
+class DEET:
+    deviation: float = 1
+    power: float = 1
+    invert: bool = False
+    compute_scale: torch.dtype | None = torch.float64
+
+    def __call__(self, low_noise: torch.Tensor, high_noise: torch.Tensor) -> torch.Tensor:
+        if self.compute_scale is None:
+            return deet(low_noise, high_noise, self.deviation, self.power, self.invert)
+        else:
+            return deet(
+                low_noise.to(dtype=self.compute_scale),
+                high_noise.to(dtype=self.compute_scale),
+                self.deviation,
+                self.power,
+                self.invert,
+            ).to(dtype=low_noise.dtype)
+
+    @contextlib.contextmanager
+    def hook_module(
+        self,
+        module: torch.nn.Module,
+        mode: DEETMode,
+        enable: bool = True,
+        residual_deet: bool = False,
+        fwd_arg: int | str = 0,
+    ) -> Generator[None, None, None]:
+        if not enable:
+            yield
+            return
+
+        deet_high_noise: torch.Tensor | None = None
+        handles: list[RemovableHandle] = []
+
+        if isinstance(fwd_arg, int):
+            arg_index, arg_key = fwd_arg, list(inspect.signature(module.forward).parameters.keys())[fwd_arg]
+        else:
+            arg_key, arg_index = fwd_arg, list(inspect.signature(module.forward).parameters.keys()).index(fwd_arg)
+
+        def deet_input(_: torch.nn.Module, args: tuple[Any, ...], kwargs: dict[str, Any]) -> tuple[Any, dict[str, Any]]:
+            nonlocal deet_high_noise
+
+            if len(args) > arg_index:
+                sample: torch.Tensor = args[arg_index]
+            else:
+                sample = kwargs[arg_key]
+
+            if deet_high_noise is not None:
+                new_sample = self(sample, deet_high_noise)
+
+                # CoW for hygeine
+                if len(args) > arg_index:
+                    args = (*args[:arg_index], new_sample, *args[arg_index + 1 :])
+                else:
+                    kwargs = kwargs | {arg_key: new_sample}
+
+                if residual_deet:
+                    sample = new_sample
+
+            deet_high_noise = sample
+
+            return args, kwargs
+
+        def deet_output_backward[T](_: torch.nn.Module, args: tuple[Any, ...], kwargs: dict[str, Any], output: T) -> T:
+            nonlocal deet_high_noise
+
+            if isinstance(output, torch.Tensor):
+                output_tensor: torch.Tensor = output
+            elif isinstance(output, MutableMapping):
+                output_key, output_tensor = next((k, v) for k, v in output.items() if isinstance(v, torch.Tensor))
+            elif isinstance(output, MutableSequence):
+                output_index, output_tensor = next((n, o) for n, o in enumerate(output) if isinstance(o, torch.Tensor))
+            else:
+                msg = f"Output type {type(output).__name__} not supported by DEET hooks"
+                raise NotImplementedError(msg)
+
+            if mode == DEETMode.OUTPUT:
+                if deet_high_noise is not None:
+                    new_output_tensor = self(output_tensor, deet_high_noise)
+
+                    # Not CoW to keep the output typing
+                    if isinstance(output, torch.Tensor):
+                        output = new_output_tensor  # type: ignore
+                    elif isinstance(output, MutableMapping):
+                        output[output_key] = new_output_tensor  # type: ignore # Guaranteed bound
+                    elif isinstance(output, MutableSequence):
+                        output[output_index] = new_output_tensor  # type: ignore # Guaranteed bound
+                    else:
+                        raise NotImplementedError
+
+                    if residual_deet:
+                        output_tensor = new_output_tensor
+
+                deet_high_noise = output_tensor
+
+            elif mode == DEETMode.BACKWARD:
+                if len(args) > arg_index:
+                    sample: torch.Tensor = args[arg_index]
+                else:
+                    sample = kwargs[arg_key]
+
+                new_output_tensor = self(output_tensor.clone(), sample.clone())
+
+                if isinstance(output, torch.Tensor):
+                    output = new_output_tensor  # type: ignore
+                elif isinstance(output, MutableMapping):
+                    output[output_key] = new_output_tensor  # type: ignore # Guaranteed bound
+                elif isinstance(output, MutableSequence):
+                    output[output_index] = new_output_tensor  # type: ignore # Guaranteed bound
+                else:
+                    raise NotImplementedError
+            else:
+                raise NotImplementedError
+
+            return output
+
+        try:
+            if mode == DEETMode.INPUT:
+                handles.append(module.register_forward_pre_hook(deet_input, with_kwargs=True, prepend=True))
+            elif mode in (DEETMode.OUTPUT, DEETMode.BACKWARD):
+                handles.append(module.register_forward_hook(deet_output_backward, with_kwargs=True))
+            yield
+        finally:
+            for h in handles:
+                h.remove()
